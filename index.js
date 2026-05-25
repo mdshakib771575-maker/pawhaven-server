0606985336be46952377b39c23e7d6489d43d839
Shakib <mdshakib771575@gmail.com>
2026-05-24 20:06:25 -0700

    add updete section

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const console = require("node:console");
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 
@@ -27,19 +28,42 @@ async function run() {
     await client.connect();
 
     const db = client.db("pawhaven");
-    const destinationCullaction = db.collection('pets');
+    const pawhavenCullaction = db.collection('pets');
 
 
     app.get("/pets", async (req, res) => {
-      const result = await destinationCullaction.find().toArray()
+      const result = await pawhavenCullaction.find().toArray()
       res.json(result)
     });
 
     app.get('/pets/:id', async (req, res) => {
       const id = req.params.id;
-      const result = await destinationCullaction.findOne({ _id: new ObjectId(id) })
+      const result = await pawhavenCullaction.findOne({ _id: new ObjectId(id) })
       res.json(result);
     });
+       app.patch('/pets/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      console.log(updatedData)
+      const result = await pawhavenCullaction.updateOne({ _id: new ObjectId(id) },{$set:updatedData})
+      res.json(result)
+    })
+     
+       app.delete('/pets/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await pawhavenCullaction.deleteOne({ _id: new ObjectId(id)});
+      res.json(result)
+    })
+
+  
+    app.post('/pets', async (req, res) => {
+      const petData = req.body;
+      console.log(petData)
+      console.log(petData)
+      const result = await pawhavenCullaction.insertOne(petData);
+      res.json(result)
+    });
+
  
 
 
